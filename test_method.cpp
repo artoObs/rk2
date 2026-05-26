4054741034d8348269a2c4f1b8b5176cddbbe35b
artoObs <petrov.maxim.2007@gmail.com>
2026-05-26 09:48:07 +0300

    Create test_method.cpp

---
+++ test_method.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include "ConcreteClassX.h"
+#include "ConcreteClassY.h"
+
+class CoutRedirect {
+    std::stringstream buffer;
+    std::streambuf* old;
+public:
+    CoutRedirect() : old(std::cout.rdbuf(buffer.rdbuf())) {}
+    ~CoutRedirect() { std::cout.rdbuf(old); }
+    std::string str() const { return buffer.str(); }
+};
+
+int main() {
+    CoutRedirect redirect;
+
+    ConcreteClassX x;
+    x.templateMethod();
+    ConcreteClassY y;
+    y.templateMethod();
+    std::string output = redirect.str();
+
+    if (output.find("PrimitiveOperationA implementation from X") == std::string>
+    if (output.find("PrimitiveOperationB implementation from X") == std::string>
+    if (output.find("PrimitiveOperationA implementation from Y") == std::string>
+    if (output.find("PrimitiveOperationB implementation from Y") == std::string>
+
+    std::cout << "All tests passed!\n";
+    return 0;
+}
